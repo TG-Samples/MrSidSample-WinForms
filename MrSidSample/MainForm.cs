f666e6703e06c0a457661bde382b637ea9078a8b
KevinLaw <kevinlaw@thinkgeo.com>
2016-11-15 11:07:43 +0800

    update data filename find logic

--- MrSidSample/MainForm.cs
+++ MrSidSample/MainForm.cs
@@ -2,31 +2,36 @@
 using System.Windows.Forms;
 using ThinkGeo.MapSuite.Layers;
 using ThinkGeo.MapSuite.WinForms;
+using System.IO;
 
 namespace ThinkGeo.MapSuite.Samples
 {
-    public partial class MainForm : Form
-    {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+	public partial class MainForm : Form
+	{
+		public MainForm ()
+		{
+			InitializeComponent ();
+		}
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-            winformsMap1.MapUnit = GeographyUnit.DecimalDegree;
+		private void MainForm_Load (object sender, EventArgs e)
+		{
+			winformsMap1.MapUnit = GeographyUnit.DecimalDegree;
 
-            LayerOverlay overlay = new LayerOverlay();
+			LayerOverlay overlay = new LayerOverlay ();
 
-            MrSidRasterLayer mrSidRasterLayer = new MrSidRasterLayer("../../App_Data/World.sid");
-            overlay.Layers.Add(mrSidRasterLayer);
+			//To resolve issue that we cannot run the executable by double click it on linux, we need to find out the absolute path by reflection.
+			string baseDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+			string mrSidLayerFileName = Path.GetFullPath (Path.Combine (baseDirectory, "../../App_Data/World.sid"));
+				 
+			MrSidRasterLayer mrSidRasterLayer = new MrSidRasterLayer (mrSidLayerFileName);
+			overlay.Layers.Add (mrSidRasterLayer);
 
-            winformsMap1.Overlays.Add(overlay);
+			winformsMap1.Overlays.Add (overlay);
 
-            mrSidRasterLayer.Open();
-            winformsMap1.CurrentExtent = mrSidRasterLayer.GetBoundingBox();
+			mrSidRasterLayer.Open ();
+			winformsMap1.CurrentExtent = mrSidRasterLayer.GetBoundingBox ();
 
-            winformsMap1.Refresh();
-        }
-    }
+			winformsMap1.Refresh ();
+		}
+	}
 }
